8beac199dc3274837fc47a3173a90567cd6d0642
Pham Cong Dinh <congdinh2395@gmail.com>
2021-05-06 22:22:23 +0700

    Add user model and validation

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /^[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+$/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
+end
